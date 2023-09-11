f203b48b22f1ab21f90c9fb27390d6cc0a30679e
kurtbravo <hell.trip@mail.ru>
2023-09-11 15:16:25 +0300

    Complete dz_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+pay_rate = 2.0
+ex = Item("Холодильник", 5000.0, 10)
+def test_calculate_total_price():
+    assert ex.price * ex.quantity == 50000.0
+    assert Item.calculate_total_price(ex) == 50000.0
+
+def test_apply_discount():
+    assert Item.apply_discount(ex) == None
